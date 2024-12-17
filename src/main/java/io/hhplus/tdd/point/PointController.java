9740ba5f03f0418661b26b8473d27204a29fbcc1
Youjin Kim <gracecancho@gmail.com>
2024-12-17 12:02:01 +0900

    프로젝트 기본 구조 짜기

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.service.PointService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -12,45 +13,55 @@ public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
 
+    private final PointService pointService;
+
+    public PointController(PointService pointService) {
+        this.pointService = pointService;
+    }
+
     /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     * 특정 유저의 포인트를 조회
      */
     @GetMapping("{id}")
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("User Point: {}", id);
+        return pointService.getUserPoint(id);
     }
 
     /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     * 특정 유저의 포인트 충전/이용 내역을 조회
      */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        log.info("User Point History: {}", id);
+        return pointService.getUserPointHistory(id);
     }
 
     /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+     * 특정 유저의 포인트를 충전하는 기능
      */
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("Charge {} points to user: {}", amount, id);
+        return pointService.chargeUserPoint(id, amount);
     }
 
     /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+     * 특정 유저의 포인트를 사용하는 기능
      */
     @PatchMapping("{id}/use")
     public UserPoint use(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("Use {} points for user: {}", amount, id);
+        return pointService.useUserPoint(id, amount);
     }
 }
---
+++ src/main/java/io/hhplus/tdd/service/PointService.java
@@ -0,0 +1,62 @@
+package io.hhplus.tdd.service;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
+import io.hhplus.tdd.point.UserPoint;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PointService {
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
+        this.userPointTable = userPointTable;
+        this.pointHistoryTable = pointHistoryTable;
+    }
+
+    // 특정 유저의 포인트를 조회
+    public UserPoint getUserPoint(long userId){
+        return userPointTable.selectById(userId);
+    }
+
+    // 특정 유저의 포인트 충전/이용 내역을 조회
+    public List<PointHistory> getUserPointHistory(long userId){
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    // 특정 유저의 포인트를 충전하는 기능
+    public UserPoint chargeUserPoint(long userId, long amount){
+        // 기존 포인트 조회
+        UserPoint userPoint = userPointTable.selectById(userId);
+        // 포인트 충전
+        long addPoint = userPoint.point()+ amount;
+        // 포인트 업데이트
+        userPointTable.insertOrUpdate(userId, addPoint);
+        // 충전 내역 기록
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        // 갱신된 포인트 반환
+        return userPointTable.selectById(userId);
+    }
+
+    // 특정 유저의 포인트를 사용하는 기능
+    public UserPoint useUserPoint(long userId, long amount){
+        // 기존 포인트 조회
+        UserPoint userPoint = userPointTable.selectById(userId);
+        // 포인트 차감
+        long addPoint = userPoint.point() - amount;
+        // 포인트가 부족하면 예외 처리
+        if (addPoint < 0) {}
+        // 포인트 업데이트
+        userPointTable.insertOrUpdate(userId, addPoint);
+        // 사용 내역 기록
+        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+        // 갱신된 포인트 반환
+        return userPointTable.selectById(userId);
+    }
+}
